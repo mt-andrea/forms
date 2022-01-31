2311e5d831ebb47f2d534227aeef4d5fc6d437ac
mt-andrea <mateandicica@gmail.com>
2022-01-31 19:26:45 +0100

    vnev change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [nev,setVnev]=useState("")
+
+  function beallit(event) {
+    setVnev(event.target.value)
+  }
+
   return (
     <div className="container">
-      <h1>Szia </h1>
+      <h1>Szia {vnev} </h1>
       <form>
-        <input name="vnev" placeholder="Vezetéknév" />
+        <input 
+        onChange={beallit}
+        name="vnev" 
+        placeholder="Vezetéknév" 
+        value={vnev}/>
         <input name="knev" placeholder="Keresztnév" />
         <button type="button">Töröl</button>
       </form>
