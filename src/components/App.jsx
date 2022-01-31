5370e09c79974d6d127cfb082328db3be62a58c2
mt-andrea <mateandicica@gmail.com>
2022-01-31 19:41:43 +0100

    full name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
 
-  const [nev,setVnev]=useState("")
+  const [teljesNev, setTeljesnev] = useState({
+    vnev: "",
+    knev: ""
+  })
 
   function beallit(event) {
-    setVnev(event.target.value)
+    const {value, name}=event.target
+
+    if (name==="vnev") {
+      setTeljesnev({
+        vnev:value,
+        knev: teljesNev.knev
+      })
+    }else if (name==="knev") {
+      setTeljesnev({
+        vnev:teljesNev.vnev,
+        knev: value
+      })
+    }
+
   }
 
   return (
     <div className="container">
-      <h1>Szia {vnev} </h1>
+      <h1>Szia {teljesNev.vnev} {teljesNev.knev} </h1>
       <form>
+        <input
+          onChange={beallit}
+          name="vnev"
+          placeholder="Vezetéknév"
+          value={vnev} 
+          />
         <input 
         onChange={beallit}
-        name="vnev" 
-        placeholder="Vezetéknév" 
-        value={vnev}/>
-        <input name="knev" placeholder="Keresztnév" />
+        name="knev" 
+        placeholder="Keresztnév"
+        value={teljesNev.knev} 
+        />
         <button type="button">Töröl</button>
       </form>
     </div>
